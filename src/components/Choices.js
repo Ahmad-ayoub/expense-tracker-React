95c1b316abb85b77f5205d3b8a38237c9808a4e2
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-04-17 00:42:34 +0300

    new updates

--- src/components/Choices.js
+++ src/components/Choices.js
@@ -1,32 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Choices() {
+function AddExpenseBtn() {
+  const [expenseInput, setExpenseInput] = useState({
+    value1: "",
+    value2: "",
+    value3: "",
+    value4: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setExpenseInput((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleAddExpense = () => {
+    console.log(expenseInput);
+  };
+  //debugger;
   return (
     <div>
       <div class="choices">
         <text>Type:</text>
-        <select name="" id="currencyType" class="selectTag">
+        <select
+          name="typeOfCurrency"
+          id="currencyType"
+          value={expenseInput.value1}
+          onChange={handleInputChange}
+          class="selectTag"
+        >
           <option value="Card">Card</option>
           <option value="Cash">Cash</option>
           <option value="Crypto">Crypto</option>
           <option value="Other">Other</option>
         </select>
         <text>Name:</text>
-        <input type="text" id="name" placeholder="What did you spend it on?" />
+        <input
+          type="text"
+          id="name"
+          placeholder="What did you spend it on?"
+          value={expenseInput.value2}
+          onChange={handleInputChange}
+        />
       </div>
 
       <div class="choices">
         <text>Date:</text>
-        <input type="date" id="date" class="selectDate" />
+        <input
+          type="date"
+          id="date"
+          class="selectDate"
+          value={expenseInput.value3}
+          onChange={handleInputChange}
+        />
         <text>Amount:</text>
-        <input type="text" id="amount" placeholder="How much?" />
+        <input
+          type="text"
+          id="amount"
+          placeholder="How much?"
+          value={expenseInput.value4}
+          onChange={handleInputChange}
+        />
       </div>
 
-      <button class="button" id="calculateExpenses">
+      <button class="button" onClick={handleAddExpense}>
         Add a new expense
       </button>
     </div>
   );
 }
 
-export default Choices;
+export default AddExpenseBtn;
