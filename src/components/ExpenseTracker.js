96a175a8fcef0eff9675c049ab285e82baac588e
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-10-24 04:58:17 -0400

    removed empty object in useState for expenses

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import Table from "./Table";
 function ExpenseTracker() {
-  const [expenses, setExpenses] = useState([]);
+  const [expenses, setExpenses] = useState();
 
   const onAddExpense = (expense) => {
     const updatedExpenses = [...expenses, expense];
